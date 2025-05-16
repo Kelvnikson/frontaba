8148bd29602edf6e220849b8e98f6213b297a4fb
KELVNIKSON SOUZA SILVA <view_k3@hotmail.com>
2025-05-16 09:58:40 -0400

    UPTADE JS2

--- server.js
+++ server.js
@@ -80,3 +80,10 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`API rodando na porta ${PORT}`);
 });
+
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err);
+});
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Unhandled Rejection:', reason);
+});
